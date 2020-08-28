3adb63c83afe70f256b659897b273e837eff0174
Ori Cohen <or.co.blog@gmail.com>
2020-08-28 10:46:55 +0300

    A commit

--- src/Components/Dashboard/Messages/Messages.js
+++ src/Components/Dashboard/Messages/Messages.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Messages.scss';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://localhost:8080';
 
 const Messages = (props) => {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+	const socket = socketIOClient(ENDPOINT);
+	socket.on('msg', data => {
+		setResponse(data.message);
+	});
+  }, []);
+
   return (
     <div>
-      The Messages page.
+      The Messages page, {response}
     </div>
   );
 }
